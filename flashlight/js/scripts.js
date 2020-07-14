62cb1296ac65dca7046a732b92064a00e4fdc1d6
mike <miguel@medible.cl>
2020-07-14 13:24:44 -0400

    Add Flashlight

---
+++ flashlight/js/scripts.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var width = 200,
+    height = 200;
+
+var log = console.log.bind(console);
+
+var containerEl = document.querySelector('.container'),
+    imgEl = containerEl.querySelector('.img');
+
+function handleMove(event) {
+
+  var eventToUse = event;
+
+  if (event.type === 'touchstart' || event.type === 'touchmove') {
+    eventToUse = event.changedTouches[0];
+    event.preventDefault(); // prevent Y-scrolling when swiping up/down
+  }
+
+  var bcr = containerEl.getBoundingClientRect();
+  var _eventToUse = eventToUse;
+  var clientX = _eventToUse.clientX;
+  var clientY = _eventToUse.clientY;
+
+  imgEl.style.left = clientX - bcr.left - width / 2 + 'px';
+  imgEl.style.top = clientY - bcr.top - height / 2 + 'px';
+}
+
+containerEl.addEventListener('mousemove', handleMove);
+containerEl.addEventListener("touchmove", handleMove, true);
